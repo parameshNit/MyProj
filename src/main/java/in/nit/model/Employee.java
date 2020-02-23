00cb5dc1c2df758661f0586d40206c4f719f4149
praveen <praveen@192.168.0.35>
2020-02-23 09:44:43 +0530

    Wh 12 rebal

---
+++ src/main/java/in/nit/model/Employee.java
@@ -0,0 +1,15 @@
+package in.nit.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Employee {
+	@Id
+	private Integer empId;
+	private String empName;
+	private Double empSal;
+
+}
